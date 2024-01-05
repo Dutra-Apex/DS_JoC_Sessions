c90b781b436a12d4293b9bc683b3cff30f094193
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2024-01-04 20:05:40 -0500

    Create discord_webscrapper.py

---
+++ Code/discord_webscrapper.py
@@ -0,0 +1,28 @@
+import json
+import requests
+
+def retrieve_messages(channel_id, authorization, filename):
+
+    headers = {
+        'authorization' : authorization
+    }
+
+    r = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages?limit=50', headers=headers)
+    json_obj = json.loads(r.text)
+ 
+    filter(json_obj)
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(json_obj, file, ensure_ascii=False, indent=4)
+
+    for value in json_obj:
+        print(value, '\n')
+
+
+auth = ''
+channel_id = ""
+filename = 'discord_messages.json'
+
+retrieve_messages(channel_id, auth, filename)
+
+# use youtube api 
